c2cc23243e3798bcf9be922fdae6d60ef056b5eb
Roni Sarkar <ronichandrasarkar@gmail.com>
2024-05-14 16:58:18 +0530

    add status bar

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {Button, Modal, StyleSheet, Text, View} from 'react-native';
-import ModalCom from './src/components/modal/modal-com';
+import {Button, Modal, StatusBar, StyleSheet, Text, View} from 'react-native';
+
 function App(): React.JSX.Element {
   return <View></View>;
 }
---
+++ src/components/status/status-bar.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {StatusBar, View} from 'react-native';
+
+const StatusBarCom = () => {
+  return (
+    <View>
+      <StatusBar backgroundColor="gray" barStyle="dark-content" />
+    </View>
+  );
+};
+
+export default StatusBarCom;
