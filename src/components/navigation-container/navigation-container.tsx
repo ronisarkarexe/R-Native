fd73396644825c81bc3806ec2b55053dcbd26606
Roni Sarkar <ronichandrasarkar@gmail.com>
2024-05-15 14:54:07 +0530

    stack navigation added

--- src/components/navigation-container/navigation-container.tsx
+++ src/components/navigation-container/navigation-container.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
